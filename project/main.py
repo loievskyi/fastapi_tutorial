fae5658c54af2f81d3174afb562580ec21ecbfc3
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-10-04 12:39:33 +0300

    Path parameters section: Add multiple endpoints to the FastAPI application, including `/users`, `/models`, and `/files` with added enum support

--- project/main.py
+++ project/main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +13,36 @@ async def root():
 @app.get("/items/{item_id}/")  # item_id is a path parameter
 async def read_item(item_id: int, query_parameter: int = 11):
     return {"item_id": item_id, "query_parameter": query_parameter}
+
+
+@app.get("/users/me")  # The order of endpoints is important.
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    asdf = "zxc"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
