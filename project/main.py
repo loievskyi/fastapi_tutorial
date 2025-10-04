918dd074c3b0e56d27c11e62e65dec8072624196
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-10-04 12:03:05 +0300

    Add the ` /items/{item_id}/ ` endpoint with a query parameter to the FastAPI application

--- project/main.py
+++ project/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}/")  # item_id is a path parameter
+async def read_item(item_id: int, query_parameter: int = 11):
+    return {"item_id": item_id, "query_parameter": query_parameter}
