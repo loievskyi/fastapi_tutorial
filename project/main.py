6166e9ca139cabe1b64b4a377922b97fd057184a
Slava Loievskyi <loievskyi.slava@gmail.com>
2025-10-04 14:47:11 +0300

    Request body section: Add POST and PUT endpoints for handling items with Pydantic model validation in the FastAPI application

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from typing import Any
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -50,9 +52,25 @@ async def read_file(file_path: str):
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 @app.get("/items/{item_id}")
@@ -65,3 +83,17 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result: dict[str, Any] = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
+
+# The function parameters will be recognized as follows:
+#
+# If the parameter is also declared in the path, it will be used as a path parameter.
+# If the parameter is of a singular type (like int, float, str, bool, etc.) it will be interpreted as a query parameter.
+# If the parameter is declared to be of the type of Pydantic model, it will be interpreted as a request body.
